cd6a401e66be25d0887c3f389f8217a4e26c6152
raizen02 <raizen02@live.com>
2015-01-22 18:53:27 +0800

    Base Copy
    First Commit

---
+++ constructMVC/constructMVC/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace constructMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult ContactUs()
+        {
+            ViewBag.Message = "Contact EECS";
+
+            return View();
+        }
+    }
+}
---
+++ constructMVC/constructMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="constructMVC.MvcApplication" Language="C#" %>
